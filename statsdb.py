03b9604d70f1414262504541cbf0c0fe29011e78
David McQueeney <davidmcq@us.ibm.com>
2018-01-15 16:10:01 -0500

    added statsdb

---
+++ statsdb.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import socket
+import time
+import os
+import platform
+
+server_address = ('10.0.0.48',10137)
+
+def statsdb(channel, value):
+    try:
+        # Create a TCP/IP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(server_address)
+        pyname = os.path.basename(__file__)
+        sockpayload = (channel + "," + str(int(time.time())) + ","
+                       + str(value) + ",DD" + "," + platform.node())
+        print("sockpayload: ", sockpayload)
+        retcode=sock.sendall(sockpayload)
+    except:
+        print ("Exception calling sock.sendall at [" + str(int(time.time())) + "]")
+    finally:
+        sock.close()
+
+    return (retcode)
+
+
+
